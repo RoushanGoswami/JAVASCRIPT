966b7bf48665b0e64556ae49038c297fc20eeab7
Roushan Goswami <roushangoswami1378@gmail.com>
2025-12-23 16:54:01 +0530

    Update homeKa.js

--- QUIZ_APP/homeKa.js
+++ QUIZ_APP/homeKa.js
@@ -1,8 +1,3 @@
 const email_input = document.getElementById("email");
 const password_input = document.getElementById("password");
 const singin_button = document.getElementById("sign-in");
-
-
-
-
-
