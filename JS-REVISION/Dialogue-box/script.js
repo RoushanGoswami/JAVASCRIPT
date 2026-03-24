5360a3a4e61bdb58dd987cfa313ceee666978731
Roushan Goswami <roushangoswami1378@gmail.com>
2026-03-24 16:00:44 +0530

    Add files via upload

---
+++ JS-REVISION/Dialogue-box/script.js
@@ -0,0 +1,8 @@
+const inputText = document.getElementById("input-text");
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", () => {
+    const value = inputText.value;//iski value leke variable me store karenge the value ko alert me display karenge
+    alert(value);
+}
+);
