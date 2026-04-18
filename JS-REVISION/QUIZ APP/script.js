98f976deed2204d7043058c6f5e0d6ce1687c320
Roushan Goswami <roushangoswami1378@gmail.com>
2026-04-18 19:00:43 +0530

    Add files via upload

--- JS-REVISION/QUIZ APP/script.js
+++ JS-REVISION/QUIZ APP/script.js
@@ -18,86 +18,86 @@ const QuestionBank = [{
     },
     "answer": "b"
 },
-{
-    "question": "Which planet is known as the Red Planet?",
-    "options": {
-        "a": "Earth",
-        "b": "Mars",
-        "c": "Jupiter",
-        "d": "Venus"
-    },
-    "answer": "b"
-},
-{
-    "question": "What is the largest ocean on Earth?",
-    "options": {
-        "a": "Atlantic Ocean",
-        "b": "Indian Ocean",
-        "c": "Pacific Ocean",
-        "d": "Arctic Ocean"
-    },
-    "answer": "c"
-},
-{
-    "question": "Who is known as the Father of the Nation in India?",
-    "options": {
-        "a": "Jawaharlal Nehru",
-        "b": "Subhas Chandra Bose",
-        "c": "Mahatma Gandhi",
-        "d": "Bhagat Singh"
-    },
-    "answer": "c"
-},
-{
-    "question": "What is the chemical symbol for Gold?",
-    "options": {
-        "a": "Ag",
-        "b": "Au",
-        "c": "Gd",
-        "d": "Go"
-    },
-    "answer": "b"
-},
-{
-    "question": "Which country hosted the 2016 Summer Olympics?",
-    "options": {
-        "a": "China",
-        "b": "Brazil",
-        "c": "UK",
-        "d": "Russia"
-    },
-    "answer": "b"
-},
-{
-    "question": "Which is the smallest continent?",
-    "options": {
-        "a": "Europe",
-        "b": "Australia",
-        "c": "Antarctica",
-        "d": "South America"
-    },
-    "answer": "b"
-},
-{
-    "question": "Who invented the telephone?",
-    "options": {
-        "a": "Thomas Edison",
-        "b": "Alexander Graham Bell",
-        "c": "Nikola Tesla",
-        "d": "Isaac Newton"
-    },
-    "answer": "b"
-},
-{
-    "question": "What is the national animal of India?",
-    "options": {
-        "a": "Lion",
-        "b": "Elephant",
-        "c": "Tiger",
-        "d": "Leopard"
-    },
-    "answer": "c"
-},
+    // {
+    //     "question": "Which planet is known as the Red Planet?",
+    //     "options": {
+    //         "a": "Earth",
+    //         "b": "Mars",
+    //         "c": "Jupiter",
+    //         "d": "Venus"
+    //     },
+    //     "answer": "b"
+    // },
+    // {
+    //     "question": "What is the largest ocean on Earth?",
+    //     "options": {
+    //         "a": "Atlantic Ocean",
+    //         "b": "Indian Ocean",
+    //         "c": "Pacific Ocean",
+    //         "d": "Arctic Ocean"
+    //     },
+    //     "answer": "c"
+    // },
+    // {
+    //     "question": "Who is known as the Father of the Nation in India?",
+    //     "options": {
+    //         "a": "Jawaharlal Nehru",
+    //         "b": "Subhas Chandra Bose",
+    //         "c": "Mahatma Gandhi",
+    //         "d": "Bhagat Singh"
+    //     },
+    //     "answer": "c"
+    // },
+    // {
+    //     "question": "What is the chemical symbol for Gold?",
+    //     "options": {
+    //         "a": "Ag",
+    //         "b": "Au",
+    //         "c": "Gd",
+    //         "d": "Go"
+    //     },
+    //     "answer": "b"
+    // },
+    // {
+    //     "question": "Which country hosted the 2016 Summer Olympics?",
+    //     "options": {
+    //         "a": "China",
+    //         "b": "Brazil",
+    //         "c": "UK",
+    //         "d": "Russia"
+    //     },
+    //     "answer": "b"
+    // },
+    // {
+    //     "question": "Which is the smallest continent?",
+    //     "options": {
+    //         "a": "Europe",
+    //         "b": "Australia",
+    //         "c": "Antarctica",
+    //         "d": "South America"
+    //     },
+    //     "answer": "b"
+    // },
+    // {
+    //     "question": "Who invented the telephone?",
+    //     "options": {
+    //         "a": "Thomas Edison",
+    //         "b": "Alexander Graham Bell",
+    //         "c": "Nikola Tesla",
+    //         "d": "Isaac Newton"
+    //     },
+    //     "answer": "b"
+    // },
+    // {
+    //     "question": "What is the national animal of India?",
+    //     "options": {
+    //         "a": "Lion",
+    //         "b": "Elephant",
+    //         "c": "Tiger",
+    //         "d": "Leopard"
+    //     },
+    //     "answer": "c"
+    // },
     // {
     //     "question": "Which gas do plants absorb from the atmosphere?",
     //     "options": {
@@ -204,42 +204,117 @@ const QuestionBank = [{
 const TimeCounter = document.getElementById("TimeCounter");
 const question = document.getElementById("Question");
 const QuestionIndex = document.getElementById("Question-index");
+
 const optionsA = document.getElementById("option-a");
 const optionsB = document.getElementById("option-b");
 const optionsC = document.getElementById("option-c");
 const optionsD = document.getElementById("option-d");
 const NextBtn = document.getElementById("Next-btn");
 
-// display Question
+const optBoxA = document.getElementById("opt-box-a");
+const optBoxB = document.getElementById("opt-box-b");
+const optBoxC = document.getElementById("opt-box-c");
+const optBoxD = document.getElementById("opt-box-d");
+
 let currentIndex = 0;
+let timer = 5;
+let intervalId;// global variable so that it can be used anywhere in this file 
+const anslist = [];
+let score = 0;
+
+// display Question
+
 function displayQuestions() {
-    QuestionIndex.textContent = "Q - " + (currentIndex + 1);
-    question.textContent = QuestionBank[currentIndex].question;
-    optionsA.textContent = QuestionBank[currentIndex].options.a;
-    optionsB.textContent = QuestionBank[currentIndex].options.b;
-    optionsC.textContent = QuestionBank[currentIndex].options.c;
-    optionsD.textContent = QuestionBank[currentIndex].options.d;
+    if (currentIndex > QuestionBank.length - 1) {
+        console.log("-----");// if condition is true then stop timer & displaying it 
+        clearInterval(intervalId);
+
+    } else {
+        startTimer();
+        QuestionIndex.textContent = "Q - " + (currentIndex + 1);
+        question.textContent = QuestionBank[currentIndex].question;
+        optionsA.textContent = QuestionBank[currentIndex].options.a;
+        optionsB.textContent = QuestionBank[currentIndex].options.b;
+        optionsC.textContent = QuestionBank[currentIndex].options.c;
+        optionsD.textContent = QuestionBank[currentIndex].options.d;
+
+        optBoxA.addEventListener("click", () => {
+            anslist.push("a");
+            console.log("a")
+            console.log(anslist)
+        });
+        optBoxB.addEventListener("click", () => {
+            anslist.push("b");
+            console.log("b")
+            console.log(anslist)
+        });
+        optBoxC.addEventListener("click", () => {
+            anslist.push("c");
+            console.log("c")
+            console.log(anslist)
+        });
+        optBoxD.addEventListener("click", () => {
+            anslist.push("d");
+            console.log("d")
+            console.log(anslist)
+        });
+    }
 }
-displayQuestions();
+
 
 // next button
 NextBtn.addEventListener("click", () => {
-    if (currentIndex < QuestionBank.length - 1) {
-        currentIndex++
+    currentIndex++
+    if (currentIndex < QuestionBank.length) {
+        displayQuestions();
+        if (currentIndex == QuestionBank.length - 1) {
+            NextBtn.textContent = "Submit";
+        }
+
+    } else {
+        // alert("All Questions are Attempted !");
+        scoreCounter();
+        showScore();
+        console.log(score);
     }
-    if (currentIndex == 9) {
-        NextBtn.textContent = "Submit";
-    }
-    displayQuestions();
 });
 
+// score Counter Function
+function scoreCounter() {
+    // anslist array -> traverse -> compare -> update score means ++
+    QuestionBank.map((que, i) => {
+        if (que.answer == anslist[i]) {
+            score++;
+        }
+    })
+}
+
+// score show function
+function showScore() {
+    const scoreShowUI = document.getElementById("score");
+    scoreShowUI.classList.add("show-score", "fade-in");
+
+    scoreShowUI.innerHTML = `
+        🎉 Quiz Completed! <br><br>
+        Your Score: <b>${score} / ${QuestionBank.length}</b> <br><br>
+        ${score === QuestionBank.length ? "🔥 Perfect Score!" : ""}
+    `;
+}
+
 // timer function
-let timer = 0;
-setInterval(() => {
-    if (timer < 10) {
-        timer++;
-        displayQuestions();
-    }
-    TimeCounter.textContent = timer;
-}, 1000);
+
+function startTimer() {
+    clearInterval(intervalId);
+    timer = 5;
+    intervalId = setInterval(() => {
+        TimeCounter.textContent = timer;
+        timer--;
+        if (timer < 0) {
+            currentIndex++;
+            displayQuestions();
+        }
+    }, 1000)
+}
+displayQuestions();
+
 
