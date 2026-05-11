f56caa551f41db4f6d7ccc4ae37f44e5d0c89b34
Roushan Goswami <roushangoswami1378@gmail.com>
2026-05-11 18:54:51 +0530

    Add files via upload

--- JS-REVISION/Task Manager/script.js
+++ JS-REVISION/Task Manager/script.js
@@ -1,3 +1,4 @@
+// Variable 
 const inputTask = document.getElementById("input-task");
 const inputPriority = document.getElementById("input-priority");
 const addTaskButton = document.getElementById("add-task-button");
@@ -9,22 +10,47 @@ const filterButton = document.getElementById("filter-button");
 const displayTasksBox = document.getElementById("display-tasks-box");
 
 const fetchTaskListFromLocalStrorage = () => {
-    return JSON.parse(localStorage.getItem("task-list")) || [];
+  return JSON.parse(localStorage.getItem("task-list")) || [];
 };
 
 const updateTaskListToLocalStorage = (taskList) => {
-    localStorage.setItem("task-list", JSON.stringify(taskList));
+  localStorage.setItem("task-list", JSON.stringify(taskList));
 }
 
 const handleAddTask = () => {
-    // Taking the values
-    const task = inputTask.value;
-    const priority = inputPriority.value;
-    const status = false;
+  // Taking the values
+  const task = inputTask.value;
+  const priority = inputPriority.value;
+  const status = false;
 
-    const taskList = fetchTaskListFromLocalStrorage();
-    taskList.push({ task, priority, status });
-    updateTaskListToLocalStorage(taskList);
+  const taskList = fetchTaskListFromLocalStrorage();
+  taskList.push({ task, priority, status });
+  updateTaskListToLocalStorage(taskList);
+}
+// Display Tasks 
+const displayTasks = () => {
+  const taskList = fetchTaskListFromLocalStrorage();// get data 
+  displayTasksBox.innerHTML = "";
+  taskList.map((task, i) => {
+    const div = document.createElement("div");
+    div.innerHTML = `   <div class="d-flex justify-content-between shadow rounded border border-light p-3">
+                <p class="fs-5">${task.task}</p>
+                <div>
+                    <button type="button" class="btn btn-warning me-2">Edit</button>
+                    <button onclick="handleDeleteTask(${i})" type="button" class="btn btn-danger">Remove</button>
+                </div>
+            </div>`
+    displayTasksBox.appendChild(div);
+  })
 }
 
-addTaskButton.addEventListener("click", handleAddTask)
+// delete Button function 
+const handleDeleteTask = (i) => {
+  const taskList = fetchTaskListFromLocalStrorage();
+  taskList.splice(i, 1);
+  updateTaskListToLocalStorage(taskList);
+  displayTasks();
+}
+
+addTaskButton.addEventListener("click", handleAddTask);
+displayTasks();
